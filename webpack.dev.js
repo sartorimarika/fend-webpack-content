b8b3a3192967a6227dfee89e558c21dee9a8bf8e
Marika Sartori <marikasartori@MacBook-Pro-di-Marika.local>
2022-03-10 23:45:54 +0100

    webpack-dev-server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/client/index.js',
@@ -19,6 +20,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new BundleAnalyzerPlugin()
     ]
 }
